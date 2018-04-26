3cc4d439700f80381653528bf8cd75be76c5b3a8
anokhipatel <asp43405@uga.edu>
2018-04-26 13:02:36 -0400

    Update DiaryBeanTest.java

--- DiaryBeanTest.java
+++ DiaryBeanTest.java
@@ -4,19 +4,22 @@ import junit.framework.*;
 import java.sql.*;
 
 public class DiaryBeanTest extends TestCase{
-    
+    //test for constructor to check if null or not    
     public void testConstructor() throws ClassNotFoundException, SQLException{
-		Posting instance = new Posting();
-		assertEquals(null, instance.getAuthor());
-		assertEquals(null, instance.getComment());
+	Posting instance = new Posting();
+	assertEquals(null, instance.getAuthor());
+	assertEquals(null, instance.getComment());
         
     }
-	public void testInsert() throws SQLException, ClassNotFoundException{
-		DiaryBean instance = new DiaryBean();
-		ArrayList <Posting> postObjList = instance.getPostingList();
-		instance.insertEntry("Anokhi", "test");
-		Posting post1 = postObjList.get(0);
-		assertEquals("Anokhi", post1.getAuthor());
-		assertEquals("test", post1.getComment());
-	}
+    //tests the insert entry to check if the same or not
+    public void testInsert() throws SQLException, ClassNotFoundException{
+	DiaryBean instance = new DiaryBean();
+	ArrayList <Posting> postObjList = instance.getPostingList();
+	instance.insertEntry("Anokhi", "Worked on making java classes");
+	Posting post1 = postObjList.get(0);
+	assertEquals("Anokhi", post1.getAuthor());
+	assertEquals("Worked on making java classes", post1.getComment());
+    }
 }
+
+
