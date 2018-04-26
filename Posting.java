10c170f53a0fcb8215e82581bf24880620547c1d
anokhipatel <asp43405@uga.edu>
2018-04-26 13:02:16 -0400

    Update Posting.java

--- Posting.java
+++ Posting.java
@@ -6,31 +6,46 @@ import java.lang.*;
 import java.io.*;
 import java.sql.*;
 
-public class Posting {
+public class Posting implements Serializable {
     String author;
     String comment;
     java.sql.Date date;
     Connection conn;
-    public Posting(){
+    //initialize constructor with no args
+    public Posting() throws ClassNotFoundException, SQLException{
         author = null;
         comment = null;
     }
-    public Posting(String authorName, String content, java.sql.Date date){
+    //constructor with args
+    public Posting(String authorName, String content, java.sql.Date date) throws SQLException{
 		this.author = authorName;
 		this.comment = content;
 		this.date = date;
     }    
+    //sets author
     public void setAuthor(String authorName){
         this.author = authorName;
     }
+    //sets comment
     public void setComment(String comment){
         this.comment = comment;
     }
-    
+    //sets the current date
+    public void setDate(java.sql.Date date){
+	this.date = date;
+    }
+    //returns author's name
     public String getAuthor(){
         return author;
     }
+    //returns the content written
     public String getComment(){
         return comment;
     }
+    //returns the current date
+    public java.sql.Date getDate(){
+	return date;
+    }
 }
+
+
