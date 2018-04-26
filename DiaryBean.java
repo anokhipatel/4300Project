331aafaca2638afaf2248cc902a059f42710db96
anokhipatel <asp43405@uga.edu>
2018-04-26 13:01:52 -0400

    Update DiaryBean.java

--- DiaryBean.java
+++ DiaryBean.java
@@ -8,6 +8,7 @@ public class DiaryBean implements Serializable {
     Connection conn;
     String author;
     String content;
+    //constructor of bean; calls driver and initializes variables
     public DiaryBean() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.jdbc.Driver");
         conn = DriverManager.getConnection("jdbc:mysql://localhost/Diary?user=root&password=csci4300&useSSL=false");
@@ -15,16 +16,19 @@ public class DiaryBean implements Serializable {
         content = null;
 
     }
+    //inserts new comments into mysql
     public void insertEntry(String author, String comment) throws SQLException{
-        PreparedStatement stmt=conn.prepareStatement("insert into diaryEntry (author, comment, date) " + " values(?, ?, NOW()");
+        PreparedStatement stmt=conn.prepareStatement("insert into diaryEntry (author, comment, date) " + " values(?, ?, NOW())");
         stmt.setString(1,author);
         stmt.setString(2, comment);
     }
-	
-	 public ArrayList<Posting> getPostingList() throws SQLException{
+
+    //returns the diaryEntry list		
+    public ArrayList<Posting> getPostingList() throws SQLException{
         ArrayList<Posting> postingList = new ArrayList<Posting>();
         PreparedStatement stmt = conn.prepareStatement("select author, comment, date from diaryEntry");
         ResultSet rs = stmt.executeQuery();
+	//repeats and prints every entry until done
         while(rs.next()){
             String authorName = rs.getString(1);
             String content = rs.getString(2);
@@ -34,3 +38,5 @@ public class DiaryBean implements Serializable {
         return postingList;
     }
 }
+
+
